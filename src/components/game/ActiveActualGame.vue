3fd6e3e3af9746d17dd4f89776a463360d4b6508
edmarktuazon <edmarktuazon03@gmail.com>
2025-12-18 13:34:16 +0800

    fix: open camera realtime to take photo

--- src/components/game/ActiveActualGame.vue
+++ src/components/game/ActiveActualGame.vue
@@ -42,15 +42,15 @@ const isCompleted = computed(() => {
         </h4>
         <label
           :class="[
-            'flex flex-col items-center justify-center gap-3 w-full px-4 py-3 border-2 border-dashed rounded-md cursor-pointer transition mt-4 h-44',
+            'flex flex-col items-center justify-center gap-4 w-full px-6 py-8 border-2 border-dashed rounded-xl cursor-pointer transition h-56',
             uploadingPrompt === nextPromptIndex
-              ? 'border-primary bg-primary/5'
-              : 'border-slate hover:border-primary hover:bg-primary/5',
+              ? 'border-primary bg-primary/10'
+              : 'border-slate hover:border-primary hover:bg-primary/10',
           ]"
         >
           <svg
             v-if="uploadingPrompt !== nextPromptIndex"
-            class="w-10 h-10 text-slate"
+            class="w-16 h-16 text-slate"
             fill="none"
             stroke="currentColor"
             viewBox="0 0 24 24"
@@ -59,10 +59,17 @@ const isCompleted = computed(() => {
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-8l-4-4m0 0L8 8m4-4v12"
+              d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"
+            />
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"
             />
           </svg>
-          <svg v-else class="w-10 h-10 text-primary animate-spin" fill="none" viewBox="0 0 24 24">
+
+          <svg v-else class="w-16 h-16 text-primary animate-spin" fill="none" viewBox="0 0 24 24">
             <circle
               class="opacity-25"
               cx="12"
@@ -77,16 +84,19 @@ const isCompleted = computed(() => {
               d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
             ></path>
           </svg>
-          <span
-            class="text-center"
-            :class="uploadingPrompt === nextPromptIndex ? 'text-primary font-medium' : 'text-slate'"
-          >
-            {{
-              uploadingPrompt === nextPromptIndex
-                ? 'Uploading your photo...'
-                : 'Hurry! Tap here to take or upload a photo!'
-            }}
-          </span>
+
+          <div class="text-center">
+            <p class="font-bold text-xl text-dark-gray">
+              {{ props.game?.prompts?.[nextPromptIndex] }}
+            </p>
+            <p
+              class="mt-3 text-lg"
+              :class="uploadingPrompt === nextPromptIndex ? 'text-primary' : 'text-slate'"
+            >
+              {{ uploadingPrompt === nextPromptIndex ? 'Taking photo...' : 'Tap to take a photo!' }}
+            </p>
+          </div>
+
           <input
             type="file"
             accept="image/*"
